9817261208d614cbd15af43bc8934f8920601be7
Darren Oliver <darren.oliver@htw.com.au>
2015-10-23 15:46:31 +1000

    db migrate and seed

---
+++ db/migrate/20151023052023_create_ideas.rb
@@ -0,0 +1,15 @@
+class CreateIdeas < ActiveRecord::Migration
+  def change
+    create_table :ideas do |t|
+      t.string :name
+      t.text :description
+      t.string :picture
+      t.timestamps null: false
+    end
+  end
+
+  def down
+    drop_table :ideas
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Idea.create(name: "Sorcha", description: "And this is the post's content.")
+Idea.create(name: "Darren", description: "And this is another post.")
+Idea.create(name: "Sinatra", description: "And this is the best post")
