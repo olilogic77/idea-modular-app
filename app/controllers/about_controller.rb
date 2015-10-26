de2b9c24177dd0c974324dfb177d13622aed2017
Nicholas Kirkwood <nkirkwood@localdirectories.com.au>
2015-10-26 15:46:13 +1000

    remove testing code

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,8 +1,6 @@
 require_relative './application_controller'
 class AboutController < ApplicationController
 
-  set :run, true
-
   get '/' do
     erb :'application/about', { :layout => :'layouts/application' }
   end
